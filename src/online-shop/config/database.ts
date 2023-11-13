ea8da394b59c79fa6bca3b4007d3715bef8caeeb
Aleh Harnizonau <aleh.harnizonau@sap.com>
2023-11-13 17:10:17 +0300

    chore: add loggers

--- src/online-shop/config/database.ts
+++ src/online-shop/config/database.ts
@@ -1,13 +1,14 @@
 import mongoose from 'mongoose';
+import logger from "../log/logger";
 
 export const connect = async () => {
     const {MONGO_URI} = process.env;
 
     try {
         await mongoose.connect(String(MONGO_URI));
-        console.log("Successfully connected to database");
+        logger.info("Successfully connected to database");
     } catch (e) {
-        console.log("DataBase connection failed");
+        logger.error("DataBase connection failed");
         console.error(e);
     }
 }
